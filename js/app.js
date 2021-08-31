99b486e646c3d65513fad466273e86bcbf572a3b
Shraiwi <40672030+shraiwi@users.noreply.github.com>
2021-08-31 14:14:01 -0500

    Added VRButton

--- js/app.js
+++ js/app.js
@@ -1,22 +1,8 @@
 /// <reference path="three.js"/>
+/// <reference path="VRButton.js"/>
 
 class VRApp {
     constructor() {
-        this.webgl = null;
-        this.sess = null;
-        //this.webgl.xr.enabled = true;
-    }
-
-    async init(sess) {
-        if (!sess) {
-            console.error("Invalid WebXR session!");
-            return;
-        }
-
-        this.sess = sess;
-
-        this.sess.addEventListener("end", this.xrEnd);
-
         this.webgl = new THREE.WebGLRenderer();
         this.webgl.setSize(window.innerWidth, window.innerHeight);
 
@@ -24,12 +10,6 @@ class VRApp {
         document.body.appendChild(this.webgl.domElement);
 
         this.webgl.xr.enabled = true;
-
-        await this.webgl.xr.setSession(this.sess);
-
-        this.webgl.setAnimationLoop(this.animate);
-
-        console.info("XR Initialized!");
     }
 
     xrEnd() {
